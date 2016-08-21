5ac281689f33a5e2338e062d04b24c2a0c3bcdd9
tangnadine1 <tangy.lovesjc@gmail.com>
2016-08-20 21:24:41 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var mapCanvas = document.getElementById("map");
+var mapOptions = {
+    center: new google.maps.LatLng(42.331427, -83.0457538), zoom: 10
+}
+var map = new google.maps.Map(mapCanvas, mapOptions);
